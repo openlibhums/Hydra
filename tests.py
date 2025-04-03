e4181bfb4773e052ba1675b5ae6531933a24e870
Mauro MSL <mauro.msl@outlook.com>
2025-04-03 20:04:03 +0100

    test: Adds test case for sibling relation

--- tests.py
+++ tests.py
@@ -28,19 +28,44 @@ class SidebarArticleLinksTest(TestCase):
             article=self.article,
         )
 
-    def test_sidebar_article_links(self):
-        # Prepare a request
+    def test_sidebar_article_links_children(self):
+        # Prepare
         request = helpers.Request(journal=self.journal1)
         context = Context({"request": request})
-
         linked_article = helpers.create_article(self.journal2)
 
         models.LinkedArticle.objects.create(
             from_article=self.article,
             to_article=linked_article,
         )
-
-        result = sidebar_article_links(context, self.article)
+        # Expect
         expected = linked_article.url
+
+        # Test
+        result = sidebar_article_links(context, self.article)
+
+        self.assertIn(expected, result)
+
+    def test_sidebar_article_links_sibling(self):
+        # Prepare
+        request = helpers.Request(journal=self.journal1)
+        context = Context({"request": request})
+        parent_article = helpers.create_article(self.journal2)
+        sibling = helpers.create_article(self.journal2)
+
+        models.LinkedArticle.objects.create(
+            from_article=parent_article,
+            to_article=self.article,
+        )
+
+        models.LinkedArticle.objects.create(
+            from_article=parent_article,
+            to_article=sibling,
+        )
+        # Expect
+        expected = sibling.url
+
+        # Test
+        result = sidebar_article_links(context, self.article)
 
         self.assertIn(expected, result)
